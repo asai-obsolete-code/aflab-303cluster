64d4b25f317ce5b2a4ce8ae8afaaaa0759520609
Masataro Asai <guicho2.71828@gmail.com>
2014-09-02 17:33:27 +0900

    implemented functional programming

--- summarize.sh
+++ summarize.sh
@@ -11,6 +11,66 @@ export time
 export mem
 export filter
 
+map (){
+    local pred=$1
+    local first=$2
+    shift 2
+    echo $($pred $first)
+    if [[ $1 != "" ]]
+    then
+        map $pred $@
+    fi
+}
+
+reduce (){                         # list of numbers
+    if [[ $3 == "" ]]
+    then
+        echo $2
+    else
+        local pred=$1
+        local first=$2
+        local second=$3
+        shift 3
+        local next=$($pred $first $second)
+        reduce $pred $next $@
+    fi
+}
+
+gensym (){ mktemp --tmpdir lambda.XXXXXXXX; }
+localdef (){ echo "$1=$2;" ;}
+localdefs (){
+    local i=0
+    for arg in $@
+    do
+        i=$(( $i + 1 ))
+        localdef $arg "\$$i"
+    done
+}
+lambda (){
+    local sym=$(gensym)
+    local args=
+    while [[ $1 != "--" ]]
+    do
+        args="$args $1"
+        shift
+    done
+    shift
+    echo "#!/bin/bash" > $sym
+    localdefs $args >> $sym
+    echo "{ $* ;}" >> $sym
+    echo "defining lambda in $sym:" >&2
+    cat $sym >&2
+    chmod +x $sym
+    echo $sym
+}
+
+gt (){ if [[ $1 -gt $2 ]] ; then echo $1 ; else echo $2 ; fi }
+lt (){ if [[ $1 -lt $2 ]] ; then echo $1 ; else echo $2 ; fi }
+ge (){ if [[ $1 -ge $2 ]] ; then echo $1 ; else echo $2 ; fi }
+le (){ if [[ $1 -le $2 ]] ; then echo $1 ; else echo $2 ; fi }
+max (){ reduce gt $@ ;}
+min (){ reduce lt $@ ;}
+
 echorun(){
     # echo $*
     $@
@@ -74,4 +134,5 @@ mapconf (){
         fi
     done
 }
+
 
