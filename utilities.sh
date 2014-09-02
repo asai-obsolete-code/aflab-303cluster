a001503496e9738903b3f2557474afe3d528d401
asai <asai@fun-lucy>
2014-09-02 09:18:22 +0900

    fixed the termination rule, enlarged room for postprocessing

--- utilities.sh
+++ utilities.sh
@@ -41,9 +41,9 @@ next(){
     cd $dir
     ppn=$(($mem / ( $maxmem / $maxcpu )))
     if [[ $ppn == 0 ]] ; then ppn=1; fi
-    # add 100MB room
-    echodo qsub -l mem=$((100000 + $mem )),pmem=$((100000 + $mem )) \
-        -l walltime=$(( 30 + $time )) \
+    # add 200MB and 300sec room
+    echodo qsub -l mem=$((200000 + $mem )),pmem=$((200000 + $mem )) \
+        -l walltime=$(( 300 + $time )) \
         -l nodes=1:ppn=$ppn \
         $(kvs mem maxmem time maxtime maxcpu cgname debug dir command optfile) \
         $(eval "echo $(< $optfile)") iterator.sh
