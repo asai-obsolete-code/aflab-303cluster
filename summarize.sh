db59528c4c475a9f6735561b8d5aeb19f914908f
Masataro Asai <guicho2.71828@gmail.com>
2014-09-03 23:48:59 +0900

    added countline

--- summarize.sh
+++ summarize.sh
@@ -71,6 +71,10 @@ le (){ if [[ $1 -le $2 ]] ; then echo $1 ; else echo $2 ; fi }
 max (){ reduce gt $@ ;}
 min (){ reduce lt $@ ;}
 
+countline (){
+    wc -l < $1
+}
+
 echorun(){
     # echo $*
     $@
