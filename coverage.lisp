de9eeb38b1571b775bd377d594be2e61f1650cc8
Masataro Asai <guicho2.71828@gmail.com>
2014-09-04 19:04:24 +0900

    updated coverage.lisp and added coverage-all

--- coverage.lisp
+++ coverage.lisp
@@ -1,10 +1,34 @@
 #! /usr/local/bin/sbcl --script
 
-(let (plist)
+(load
+ (merge-pathnames
+             "quicklisp/setup"
+             (user-homedir-pathname)))
+
+(ql:quickload :iterate) (use-package :iterate)
+(ql:quickload :optima)  (use-package :optima)
+(ql:quickload :alexandria) (use-package :alexandria)
+
+
+
+(let (plist solvers domains)
   (handler-case
-      (tagbody
-        start
-        (let ((read (read *standard-input* nil nil nil)))
-          (incf (getf plist (first read) 0)))
-        (go start))
-    (print plist)))
+      (iter (match (read *standard-input*)
+              ((list* domain _ solver _)
+               (incf (getf (getf plist solver) domain 0))
+               (pushnew domain domains)
+               (pushnew solver solvers))))
+    (end-of-file (c)
+      (format t "~&| ~30a|~{ ~12a|~}" 'domains solvers)
+      (iter (for domain in domains)
+            (format t "~&| ~30a|" domain)
+            (iter (with coverages =
+                        (mapcar (lambda (solver)
+                                  (getf (getf plist solver) domain 0))
+                                solvers))
+                  (for coverage in coverages)
+                  (format t " ~12@<~:[~a~;*~a*~]~>|"
+                          (= coverage (reduce #'max coverages))
+                          coverage))))))
+
+(terpri)
