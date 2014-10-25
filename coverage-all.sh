26e335c904c08a93636cd5058171bad40e7f2852
Masataro Asai <guicho2.71828@gmail.com>
2014-10-25 13:01:06 +0900

    do not output <900

--- coverage-all.sh
+++ coverage-all.sh
@@ -2,10 +2,10 @@
 # start
 cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 3600 2> /dev/null
 cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 1800 2> /dev/null
-cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 900 2> /dev/null
-cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 450 2> /dev/null
-cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 225 2> /dev/null
-cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 112 2> /dev/null
-cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 56 2> /dev/null
-cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 28  2> /dev/null
-cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 14 2> /dev/null
+# cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 900 2> /dev/null
+# cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 450 2> /dev/null
+# cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 225 2> /dev/null
+# cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 112 2> /dev/null
+# cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 56 2> /dev/null
+# cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 28  2> /dev/null
+# cat *.summary | $(dirname $(readlink -ef $0))/coverage.lisp 14 2> /dev/null
