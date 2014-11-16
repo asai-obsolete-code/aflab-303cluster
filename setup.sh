a814a80cdbda949a29f9ec8b25d46a9b676b90ee
Masataro Asai <guicho2.71828@gmail.com>
2014-11-16 16:40:52 +0900

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+install (){
+    ../$1/install.sh > $1.list
+    cat .gitignore $1.list | sort -u > tmpignore
+    mv -f tmpignore .gitignore
+    rm $1.list
+}
+
+install doubling
+install aaai15-config
+
+cp ~/repos/lisp/pddl.component-planner/component-planner ./
+git submodule init
+git submodule update
+
