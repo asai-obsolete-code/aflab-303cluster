b079c05954825d3a1c7584a68129d7c21076968d
asai <asai@fun-lucy>
2014-11-27 12:17:24 +0900

    updated watch

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch -d -t "( ( qstat -q | tail -n 5 | head -n 3 | grep -v '\-\{3\}' ) ; ( qstat -a | grep batch | grep R | head -n 48 ) ; ( pbsnodes | grep '^fun\|jobs =' ) )"
+watch -d -t ./status.sh
