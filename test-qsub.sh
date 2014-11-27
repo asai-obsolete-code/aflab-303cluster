d561e582044343680b6d1e6e0df68ced2685b160
asai <asai@fun-lucy>
2014-11-27 12:17:38 +0900

    fixed test-qsub

--- test-qsub.sh
+++ test-qsub.sh
@@ -4,6 +4,6 @@ for host in $(cat $(dirname $0)/hosts | sed -e "s/#.*//g")
 do
     for i in $(seq 0 7)
     do
-        echo "sleep 20; hostname" | qsub -l host=$host -o $host.$i.o -e $host.$i.e -
+        echo "sleep 3; hostname" | qsub -l walltime=30,host=$host -o $host.$i.o -e $host.$i.e -
     done
 done
