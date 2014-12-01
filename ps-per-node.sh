dce7d5bfbfbf4d76268e13d06a78bacadc9310ff
asai <asai@fun-lucy>
2014-12-01 20:14:31 +0900

    bugfix (as per #2)

--- ps-per-node.sh
+++ ps-per-node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps -u asai -o pid,ppid,pgid,%cpu,%mem,rss,vsz,etime,s,comm --sort=-%mem | head -n 5 | tail -n +2
+ps -u $(whoami) -o pid,ppid,pgid,%cpu,%mem,rss,vsz,etime,s,comm --sort=-%mem | head -n 5 | tail -n +2
