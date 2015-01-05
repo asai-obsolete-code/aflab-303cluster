8fbc2c38c59b3dad33dfaf5a11c936cfbf293a17
asai <asai@fun-lucy>
2015-01-06 01:50:05 +0900

    alternative hosts

--- test/test-map.sh
+++ test/test-map.sh
@@ -20,4 +20,16 @@ echodo ../map --lucy -- -V -- ls -la
 
 # sudo
 read -p "enter sudo pass: " passwd
-echodo ../map -s $passwd -- ls -la
+echodo ../map -s $passwd whoami
+echodo ../map -s $passwd -- whoami
+echodo ../map -s $passwd -- -- whoami
+
+# symlink'd map script (default hosts)
+echodo ./map ls
+
+# symlink'd map script (hosts/hosts, fun000 only)
+echodo hosts/map ls
+
+# symlink'd map script (alternative host)
+echodo ./map -h hosts2 ls
+
