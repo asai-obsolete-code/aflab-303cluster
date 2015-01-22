c22149e4bf06497f1756f3cf650191eeb515c4c3
Masataro Asai <guicho2.71828@gmail.com>
2015-01-22 19:14:46 +0900

    fixed the number of sharps in status.sh

--- status.sh
+++ status.sh
@@ -9,21 +9,35 @@ printdots (){
     do
         echo -n \#
     done
-    for ((i=$1;i<$2;i++))       # $2 = max
+}
+
+withwidth (){
+    w=$1
+    n=$(($(echo $2 | wc -c)-1))
+    c=${3:-" "}
+    echo -n $2
+    for ((i=$n;i<$w;i++))       # $2 = max
     do
-        echo -n " "
+        echo -n "$c"
     done
 }
 
-echo "              jobs       mem"
+withwidth 20 ""
+withwidth 11 "jobs"
+withwidth 12 "mem"
+echo
 
 for h in $hosts
 do
-    jobs=$(pbsnodes $h | grep "jobs = " | tr -cd , | wc -c)
+    jobs=$(pbsnodes $h | grep "jobs = " | tr -cd / | wc -c)
     state=$(pbsnodes $h | awk '/state/{print $3; exit 0}')
-    echo "$h($state): |$(printdots $jobs 8) $(ssh $h free -h | awk '/-\/\+/{printf("%s/%s",$3,$4)}')"
+    withwidth 20 "$h($state):"
+    echo -n "["
+    withwidth 8 "$(printdots $jobs 8)"
+    echo -n "] "
+    withwidth 12 $(ssh $h free -h | awk '/-\/\+/{printf("%s/%s",$3,$4)}')
+    echo
 done
 
-echo ----------------------------------------------------------------------------------
 qstat -a | tail -n +3 | head -n 3
 qstat -a | awk '/.*batch.*/{if (match(/R/,$10)) ; { print $0 }}'
