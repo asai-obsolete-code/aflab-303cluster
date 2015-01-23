3fbf16923b283c0cee2e57e5a86b210b86a2898d
Masataro Asai <guicho2.71828@gmail.com>
2015-01-24 00:20:34 +0900

    watch.sh : added sudo mode

--- watch.sh
+++ watch.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-watch -d -t $(dirname $(readlink -ef $0))/status.sh
+lib=$(dirname $(readlink -ef $0))/watchlib.sh
+. $lib
+
+sudo=false
+
+while getopts "s" opt
+do
+    case ${opt} in
+        s) 
+            sudo=true
+            read -s -p "sudo password: " password ;;
+        \?) OPT_ERROR=1; break;;
+        * ) echo "unsupported option $opt" ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+watch=${watch:-true}
+
+if $watch
+then
+    export watch=false
+    watch -d -t $0 $@
+else
+    cluster-status
+    if $sudo
+    then
+        echo $password | sudo -S bash -c ". $lib ; job-status"
+    else
+        job-status
+    fi
+fi
---
+++ watchlib.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+. $(dirname $(readlink -ef $0))/util/gethosts.sh
+hosts=$(cat $HOSTSFILE | sed -e "s/#.*//g")
+
+printdots (){
+    for ((i=0;i<$1;i++))
+    do
+        echo -n \#
+    done
+}
+
+withwidth (){
+    w=$1
+    n=$(($(echo $2 | wc -c)-1))
+    c=${3:-" "}
+    echo -n $2
+    for ((i=$n;i<$w;i++))       # $2 = max
+    do
+        echo -n "$c"
+    done
+}
+
+cluster-status (){
+    withwidth 30 ""
+    withwidth 11 "jobs"
+    withwidth 12 "mem"
+    echo
+    for h in $hosts
+    do
+        jobs=$(pbsnodes $h | grep "jobs = " | tr -cd / | wc -c)
+        state=$(pbsnodes $h | awk '/state/{print $3; exit 0}')
+        withwidth 30 "$h($state):"
+        echo -n "["
+        withwidth 8 "$(printdots $jobs 8)"
+        echo -n "] "
+        freetext=$(ssh $h free -h | awk '/-\/\+/{printf("used: %s free: %s",$3,$4)}')
+        withwidth 25 "$freetext"
+        echo
+    done
+}
+
+job-status (){
+    qstat -q | awk '/batch/{printf("Running: %s Pending: %s\n",$6,$7)}'
+    qstat -a | tail -n +3 | head -n 3
+    qstat -a | awk '/.*batch.*/{if (match(/R/,$10)) ; { print $0 }}' | head -n 60
+}
