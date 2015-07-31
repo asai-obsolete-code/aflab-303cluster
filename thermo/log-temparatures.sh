ddc55a0279311db9c270d8384f93cc468a42273e
Masataro Asai <guicho2.71828@gmail.com>
2015-07-31 10:56:05 +0900

    parallel

--- thermo/log-temparatures.sh
+++ thermo/log-temparatures.sh
@@ -2,7 +2,7 @@
 
 scrdir=$(readlink -ef $(dirname $0)/../)
 
-$scrdir/map -l -h $scrdir/hosts.room313 "$scrdir/thermo/per-node.sh"
+$scrdir/map -l -p -h $scrdir/hosts.room313 "$scrdir/thermo/per-node.sh"
 
 # $scrdir/map -l -h $scrdir/hosts.room313 "sensors ; smartctl -A /dev/sda | grep Temperature_Celsius"
 
