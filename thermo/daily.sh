4bcb6f8006a5d01ef2e57ddc71c8562906a353d7
Masataro Asai <guicho2.71828@gmail.com>
2015-08-02 16:34:47 +0900

    added daily.sh (for shared directory)

---
+++ thermo/daily.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRDIR=$(readlink -ef $(dirname $0)/../)
+
+d1=/home/asai/Dropbox/room313-thermo-log
+d2=/home/asai/Dropbox/FukunagaLabShare/room313-thermo-log
+mkdir -p $d1 $d2
+
+cp -u -t $d2 $d1/*
