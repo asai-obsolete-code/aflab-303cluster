e8e4490051b61c7244e17031a5a65caaafe9a36f
Masataro Asai <guicho2.71828@gmail.com>
2015-08-02 19:10:15 +0900

    log number of jobs

--- thermo/log-temparatures.sh
+++ thermo/log-temparatures.sh
@@ -6,3 +6,10 @@ $scrdir/map -l -p -h $scrdir/hosts.room313 "$scrdir/thermo/per-node.sh"
 
 # $scrdir/map -l -h $scrdir/hosts.room313 "sensors ; smartctl -A /dev/sda | grep Temperature_Celsius"
 
+getjobs(){
+    qstat -B -f | grep total_jobs | awk '{print $3}'
+}
+
+echo "funlucy jobs torque \"$(date -R)\" $(date +%s) $(getjobs)" >> funlucy.log
+
+
