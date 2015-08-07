a9093711a9f029e90683a81769d443a1aa40bf92
Masataro Asai <guicho2.71828@gmail.com>
2015-08-07 10:38:46 +0900

    shutdown.sh: added immediate flag as the second argument

--- wol/shutdown.sh
+++ wol/shutdown.sh
@@ -2,6 +2,15 @@
 
 SCRDIR=$(readlink -ef $(dirname $0)/../)
 HOSTS=$SCRDIR/hosts.$1
+[ -z $1 ] && { cat <<EOF
+Usage: shutdown.sh ROOM [immediate]
+
+ROOM : room313, room502, room313-summer (fun001,fun002).
+immediate : true or false. if false, wait for the nodes to stabilize wrto NFS. 
+EOF
+    exit 1 ;}
+
+immediate=${2:-false}
 
 pbsnodes -o $(sed $HOSTS -e 's/#.*//g')
 
@@ -13,6 +22,6 @@ do
 done
 
 # wait for NFS sync
-sleep 5m
+$immediate && sleep 5m
 
 $SCRDIR/map -h $HOSTS shutdown -P now
