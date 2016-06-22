b3251a8455171df188d8d69ded8daee2dc721795
Masataro Asai <guicho2.71828@gmail.com>
2016-06-22 16:41:41 +0900

    filter existing ips

--- add-all-nodes.sh
+++ add-all-nodes.sh
@@ -1,13 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash
 
 log=$(ls /var/spool/torque/server_logs/* | sort -h | tail -n 1)
 
-grep "svr_is_request" $log | \
-    cut -d";" -f6 | \
-    awk '{print $7}' | \
-    cut -d: -f1 | \
-    sed 's/\./-/g' | \
-    awk '{print "ip-"$0}' | sort | uniq | \
-    while read line ; do
-    qmgr -c "create node $line np=18"
+echo "searching for unknown ips..."
+
+new_ips=$(
+    comm -1 -3 \
+        <(pbsnodes | grep ^ip- | sort) \
+        <({
+            grep "svr_is_request" $log | \
+                cut -d";" -f6 | \
+                awk '{print $7}' | \
+                cut -d: -f1 | \
+                sed 's/\./-/g' | \
+                awk '{print "ip-"$0}' | sort | uniq
+            }))
+
+
+
+for ip in $new_ips ; do
+    echo "new ip found! : $ip"
+    qmgr -c "create node $ip np=18"
 done
